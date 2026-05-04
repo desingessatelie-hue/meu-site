048bd26df32bc485c69f19fa8375e624dac260c7
desingessatelie-hue <desingessatelie@gmail.com>
2026-05-04 18:36:12 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -6,6 +6,13 @@ import React, { useState } from "react";
 export default function App() {
   const [categoriaAtiva, setCategoriaAtiva] = useState(null);
   const [subcategoriaAtiva, setSubcategoriaAtiva] = useState(null);
+  const categoriaSelecionada = categorias.find(
+  (cat) => cat.titulo === categoriaAtiva
+);
+
+const subcategoriaSelecionada = categoriaSelecionada?.subcategorias?.find(
+  (sub) => sub.titulo === subcategoriaAtiva
+);
 
   const numeroWhatsApp = "5547996588988";
 
